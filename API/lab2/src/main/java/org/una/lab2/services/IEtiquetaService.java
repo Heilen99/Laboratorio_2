9d42e1ba78ee73e432a1f530d496a52dfb72d4e9
Diana2801 <70043699+Diana2801@users.noreply.github.com>
2020-10-03 17:03:55 -0600

    CreaciondeEtiqueta_Reaccion_TiposArchivo
    Se crean las entidades de Etiqueta, Reacciones y Tipos de Archivos, cada una con sus respectivos paquetes

---
+++ API/lab2/src/main/java/org/una/lab2/services/IEtiquetaService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.lab2.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.una.lab2.entities.Etiqueta;
+
+/**
+ *
+ * @author Diana Acuña
+ */
+public interface IEtiquetaService {
+    public Optional<List<Etiqueta>> findAll();
+
+    public Optional<Etiqueta> findById(Long id);
+    
+}
